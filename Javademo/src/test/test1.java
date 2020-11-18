ee295eb2217e4f3088fbef58e719c1712e1b7a68
admin <admin@qq.com>
2020-11-18 15:43:38 +0800

    这是我的第二次提交

--- Javademo/src/test/test1.java
+++ Javademo/src/test/test1.java
@@ -1,9 +1,19 @@
 package test;
 
+import org.junit.Test;
+
 public class test1 {
 	public static void main(String[] args) {
 		//打印一个helloworld
 		System.out.println("hello world!");
 	}
 
+
+	/*
+	这是个单元测试
+	 */
+	@Test
+	public void fun1(){
+		System.out.println("9989");
+	}
 }
